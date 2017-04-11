3079b6a519393c0a3a4b5f5e2da656bd863d9956
Patrick Hudgell <patrickhudgell@localhost.localdomain>
2017-04-11 19:26:39 +1000

    Commit index.js

--- client/src/storage/index.js
+++ client/src/storage/index.js
@@ -1,7 +1,9 @@
 const Backbone = require('backbone');
 const eventsData = require('./events.json');
+const dummyData = require('./dummyData.json');
 
 function Storage() {
+  this.dummyData = dummyData;
   this.eventsData = new Backbone.Collection();
   this.eventsData.add(eventsData.events);
 }
