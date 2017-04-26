963c0794ccb25ff760c7ce631112da0b24830dba
Patrick Hudgell <pat.hudgell@yahoo.com.au>
2017-04-26 20:47:27 +1000

    final commit, tests passing

--- client/src/js/pages/alertPage.js
+++ client/src/js/pages/alertPage.js
@@ -2,6 +2,11 @@ const Page = require('watch_framework').Page;
 
 const alertPage = Page.extend({
   id: 'alerts',
+  render() {
+    this.$el.html('This is a demo');
+    return this;
+  },
 });
+
 
 module.exports = alertPage;
